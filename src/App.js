a0fbca82028b320d25d551575423bf44cd1c3446
gregorymark254 <gregorymark254@gmail.com>
2023-06-09 10:54:03 +0300

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import Nav from './pages/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import Pricing from './pages/Pricing'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Footer from './pages/Footer'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div >
-      home
+    <div>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> 
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
